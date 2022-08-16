386338058e0c042193b4dbee826ee4e0bf1fdcb5
Максим Радченко <tagr2101@yandex.ru>
2022-08-16 20:19:38 +0300

    Исправлены замечания.

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render, get_object_or_404
+
 from .models import Post, Group
+
+LAST_POSTS: int = 10
 
 
 def index(request):
-    posts = Post.objects.order_by('-pub_date')[:10]
+    posts = Post.objects.select_related('group')[:LAST_POSTS]
     context = {
         'posts': posts,
     }
@@ -12,7 +15,7 @@ def index(request):
 
 def group_posts(request, slug):
     group = get_object_or_404(Group, slug=slug)
-    posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
+    posts = group.posts.all()[:LAST_POSTS]
     context = {
         'group': group,
         'posts': posts,
